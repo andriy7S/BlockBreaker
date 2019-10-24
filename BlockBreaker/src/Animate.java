02b722ac3ac49655837f8b8c76c4eca4be051f71
Andriy Shapoval <andriylearn@yahoo.ca>
2019-10-24 00:55:01 -0400

    First

---
+++ BlockBreaker/src/Animate.java
@@ -0,0 +1,18 @@
+
+public class Animate implements Runnable {
+	private BlockBreakerPanel bp;
+
+	Animate(BlockBreakerPanel b) {
+		bp = b;
+	}
+	public void run() {
+		while(true) {
+			bp.update();
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
